1870adcd599b49cc145095be4d6a91df2382dc07
I-am-Miguel <miguelwelligton@gmail.com>
2018-05-09 19:08:18 -0300

    starting anime project

---
+++ src/main/kotlin/br/imd/kotlinApi/kotlinapi/KotlinApiApplication.kt
@@ -0,0 +1,11 @@
+package br.imd.kotlinApi.kotlinapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinApiApplication
+
+	fun main(args: Array<String>) {
+	    runApplication<KotlinApiApplication>(*args)
+	}
